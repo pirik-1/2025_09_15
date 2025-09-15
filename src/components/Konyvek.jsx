7fc09cc2fdd95aa19bc763fc6058cc86f0548350
Piri Kristóf <kingsroas1@gmail.com>
2025-09-15 12:59:30 +0200

    minden vilagos

--- src/components/Konyvek.jsx
+++ src/components/Konyvek.jsx
@@ -1,5 +1,6 @@
 import GyurukUra from "./GyurukUra.jsx"
 import Konyv from "./Konyv.jsx"
+import Leiras from "./Leiras.jsx";
 
 function Konyvek(){
     return(
@@ -7,6 +8,11 @@ function Konyvek(){
             <h2>Kedvenc könyveim</h2>
             <GyurukUra/>
             <Konyv szerzo = "Fekete István" cim="Tüskevár"  />
+            <Leiras>
+                <strong>
+                    A Tüskevár egy ifjúsági regény, amelyet Fekete István írt. A történet két városi fiú, Tutajos és Bütyök nyári kalandjait követi nyomon a Balaton-felvidéken, ahol megismerkednek a természet szépségeivel és a vidéki élet kihívásaival. A regény bemutatja a barátság, a bátorság és a kitartás értékeit, miközben a fiúk felfedezik a természet csodáit és megtanulják tisztelni azt.
+                </strong>
+            </Leiras>
         </>
     )
 }
---
+++ src/components/Leiras.jsx
@@ -0,0 +1,10 @@
+function Leiras(props) {
+    return(
+        <>
+            <div style={{border: "1px solid gray"}}>
+                {props.children}
+            </div>
+        </>  
+    )
+}
+export default Leiras;
