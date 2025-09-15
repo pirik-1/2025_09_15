c5605c71c1e238ea2c98e35bb0a94ad89871018c
Piri Kristóf <kingsroas1@gmail.com>
2025-09-15 12:27:04 +0200

    valami tortent

---
+++ src/components/GyurukUra.jsx
@@ -0,0 +1,12 @@
+
+function GyurukUra(){
+    return (
+        <>
+            <p>
+                <span>J.R.R. Tolkien</span>:&nbsp;
+                <span>A gyűrűk ura</span>
+            </p>
+        </>
+    )
+}
+export default GyurukUra;
---
+++ src/components/Konyv.jsx
@@ -0,0 +1,11 @@
+function Konyv(props){
+    return(
+        <>
+            <p>
+                <span>{props.szerzo}</span>: &nbsp;
+                <span>{props.cim}</span>
+            </p>
+        </>
+    )
+}
+export default Konyv;
